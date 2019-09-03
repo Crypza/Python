cbb29e66cc153af9b962ecd3087f3e3903396bdc
Crypza <tor.eriksson@elev.ga.ntig.se>
2019-09-03 10:08:03 +0200

    Lektion 1 19-09-03

---
+++ lek2.py
@@ -0,0 +1,10 @@
+print("peepeepoopoo")
+
+firstName = "Tor"
+lastName = "Eriksson"
+
+#dateOfBirth = "20011020"
+
+age = 17
+teacher= True
+print(firstName + " " + lastName + " " + str(age) + str(teacher))
