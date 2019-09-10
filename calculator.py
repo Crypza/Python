c19596374f7e372945bfe04c685260368694e4be
Crypza <tor.eriksson@elev.ga.ntig.se>
2019-09-10 10:37:37 +0200

    Mer avancerad miniräknare

--- calculator.py
+++ calculator.py
@@ -1,5 +1,30 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = int(num1) + int(num2)
+operator = input("Do you want to +, -, / or *?: ")
 
-print (result)
+try:
+    num1 = int(input("Enter a number here: "))
+    num2 = int(input("Enter another number here: "))
+except:
+    print("Thats not a number >:C")
+    exit()
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num1 == 0:
+        print("You cant divide by 0")
+        exit("Bye bye")
+    elif num2 == 0:
+        print("You cant divide by 0")
+        exit("Bye bye")
+    else:
+        result = num1 / num2
+#    result = num1 / num2
+else:
+    print("Error: not acceptable way of calculating")
+    exit()
+
+print(result)
